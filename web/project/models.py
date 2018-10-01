c7c4fa8fd533ca9d620d9c748d26584c51ea82ac
margaret.premru <premrume@gmail.com>
2018-10-01 15:09:09 -0400

    additions to db

--- web/project/models.py
+++ web/project/models.py
@@ -11,8 +11,14 @@ class Paper(db.Model):
     uuid_filename = db.Column(db.String, default=None, nullable=True)
     posted_url = db.Column(db.String, default=None, nullable=True)
     posted_timestamp = db.Column(db.DateTime, nullable=True)
+    component = db.Column(db.String(36))
+    status = db.Column(db.String(36))
+    msg = db.Column(db.String(36))
 
     def __init__(self, id, title, f1, u1, f2 ):
+        self.status = 'UPLOAD'
+        self.component = 'magpie'
+        self.msg = 'Work in progress'
         self.id = id
         self.title = title
         self.orig_filename = f1
